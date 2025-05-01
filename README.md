3701ade9ed12a0857ce7e400d65447c689111be5
Jeff Long <banterability@users.noreply.github.com>
2025-05-01 12:36:58 -0500

    install script

--- README.md
+++ README.md
@@ -3,10 +3,7 @@
 ## Setup
 
 ```
-ln -s ./gitconfig ~/.gitconfig
-ln -s ./gitignore ~/.gitignore
-ln -s ./tmux.conf ~/.tmux.conf
-ln -s ./zshrc ~/.zshrc
+./install.sh
 ```
 
 Various bits of this config assume other tools (like [`delta`](https://github.com/dandavison/delta) and [`mise`](https://mise.jdx.dev/)) are available on the system.
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Linking dotfiles…"
+
+# get the directory this script is in, regardless
+# of where it's being run from
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+files=(gitconfig gitignore tmux.conf zshrc)
+for file in "${files[@]}"; do
+  ln -sf "$dir/$file" "$HOME/.$file"
+done
+
+echo "Done."
